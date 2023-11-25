8cad3ee99e0eb92886dd0623bf3f75ae868f732d
ahsan <makabir98@yahoo.com>
2023-11-25 12:15:00 +0600

    feat/access log and error log functionality added

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const { logger } = require('./middleware/log/logEvents')
+
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(logger)
 
 app.use("/", (req, res) => {
     res.send("Hello ERP")
