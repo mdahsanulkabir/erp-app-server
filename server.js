7f6cc793619f66443da5cc3ba07f5c5456af6469
ahsan <makabir98@yahoo.com>
2024-05-21 20:20:59 +0600

    edited

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const credentials = require("./middleware/credential/credentials.js");
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient();
 
-// const PORT = process.env.PORT || 5000;
 const PORT = process.env.PORT;
 
 console.log("mode: ",process.env.MODE)
