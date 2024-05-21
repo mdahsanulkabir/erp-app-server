8cb599092dedcbd68ca96d4654e0c0f7d3c10fcd
ahsan <makabir98@yahoo.com>
2024-05-21 19:58:27 +0600

    edited

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient();
 
 // const PORT = process.env.PORT || 5000;
-const PORT = 8000;
+const PORT = process.env.PORT;
+
+console.log("mode: ",process.env.MODE)
 
 app.use(logger);
 app.use(credentials); //for frontend
